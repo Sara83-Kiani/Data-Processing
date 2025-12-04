b093b0d4c67e25018dfc4d1421b35820c851c83c
beyza ölmez <beyza.olmez@student.nhlstenden.com>
2025-12-04 18:21:58 +0100

    added and updated necessary tables, added check constrains

--- db-init/streamflix.sql
+++ db-init/streamflix.sql
@@ -1,134 +1,230 @@
--- 1️ Genre
+-- Genre
 CREATE TABLE IF NOT EXISTS `Genre` (
     genre_id INT(11) AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     PRIMARY KEY(genre_id)
 );
 
--- 2️ Subscription
+-- Classification
+CREATE TABLE IF NOT EXISTS `Classification` (
+    classification_id INT(11) AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    PRIMARY KEY(classification_id)
+);
+
+-- Subscription
 CREATE TABLE IF NOT EXISTS `Subscription` (
     subscription_id INT(11) AUTO_INCREMENT,
     description VARCHAR(255) NOT NULL,
-    subscription_price FLOAT NOT NULL DEFAULT 7.99,
+    price DOUBLE NOT NULL DEFAULT 7.99,
+    quality ENUM('SD', 'HD', 'UHD') NOT NULL DEFAULT 'SD',
     PRIMARY KEY(subscription_id)
 );
 
--- 3️ Preference & PreferenceValue
-CREATE TABLE IF NOT EXISTS `Preference` (
-    preference_id INT(11) AUTO_INCREMENT,
-    description VARCHAR(255) NOT NULL,
-    PRIMARY KEY(preference_id)
-);
-
-CREATE TABLE IF NOT EXISTS `PreferenceValue` (
-    preference_value_id INT(11) AUTO_INCREMENT,
-    preference_id INT(11) NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    PRIMARY KEY(preference_value_id),
-    FOREIGN KEY(preference_id) REFERENCES Preference(preference_id) ON UPDATE CASCADE ON DELETE NO ACTION
-);
-
--- 4️ Movie
-CREATE TABLE IF NOT EXISTS `Movie` (
-    movie_id INT(11) AUTO_INCREMENT,
-    genre_id INT(11) NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    duration TIME NOT NULL DEFAULT '00:00:00',
-    PRIMARY KEY(movie_id),
-    FOREIGN KEY(genre_id) REFERENCES Genre(genre_id) ON UPDATE CASCADE ON DELETE NO ACTION
-);
-
--- 5️ Serie
-CREATE TABLE IF NOT EXISTS `Serie` (
-    serie_id INT(11) AUTO_INCREMENT,
-    genre_id INT(11) NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    seasons INT(11) NOT NULL DEFAULT 1,
-    PRIMARY KEY(serie_id),
-    FOREIGN KEY(genre_id) REFERENCES Genre(genre_id) ON UPDATE CASCADE ON DELETE NO ACTION
-);
-
--- 6️ Episode
-CREATE TABLE IF NOT EXISTS `Episode` (
-    episode_id INT(11) AUTO_INCREMENT,
-    serie_id INT(11) NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    duration TIME NOT NULL DEFAULT '00:00:00',
-    season_id INT(11) NOT NULL,
-    PRIMARY KEY(episode_id),
-    FOREIGN KEY(serie_id) REFERENCES Serie(serie_id) ON UPDATE CASCADE ON DELETE NO ACTION
-);
-
--- 7️ User
-CREATE TABLE IF NOT EXISTS `User` (
-    user_id INT(11) AUTO_INCREMENT NOT NULL,
-    user_email VARCHAR(50) UNIQUE NOT NULL,
-    user_password VARCHAR(60) NOT NULL,
-    is_activated BOOLEAN,
-    subscription_date TIMESTAMP,
+-- Account
+CREATE TABLE IF NOT EXISTS `Account` (
+    account_id INT(11) AUTO_INCREMENT NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(60) NOT NULL,
+    is_activated BOOLEAN NOT NULL DEFAULT FALSE,
+    registration_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     subscription_id INT(11) NOT NULL,
     payment_method VARCHAR(255) NOT NULL,
-    PRIMARY KEY(user_id),
-    FOREIGN KEY(subscription_id) REFERENCES Subscription(subscription_id) ON UPDATE CASCADE ON DELETE NO ACTION
+    is_blocked BOOLEAN NOT NULL DEFAULT FALSE,
+    PRIMARY KEY (account_id),
+    FOREIGN KEY (subscription_id) 
+        REFERENCES Subscription(subscription_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
 
--- 8️ UserPreference
-CREATE TABLE IF NOT EXISTS `UserPreference` (
-    user_preference_id INT(11) AUTO_INCREMENT,
-    user_id INT(11) NOT NULL,
-    preference_id INT(11) NOT NULL,
-    preference_value_id INT(11) NOT NULL,
-    PRIMARY KEY(user_preference_id),
-    FOREIGN KEY(user_id) REFERENCES User(user_id) ON UPDATE CASCADE ON DELETE NO ACTION,
-    FOREIGN KEY(preference_id) REFERENCES Preference(preference_id) ON UPDATE CASCADE ON DELETE NO ACTION,
-    FOREIGN KEY(preference_value_id) REFERENCES PreferenceValue(preference_value_id) ON UPDATE CASCADE ON DELETE NO ACTION
-);
-
--- 9️ Profile
+-- Profile
 CREATE TABLE IF NOT EXISTS `Profile` (
     profile_id INT(11) AUTO_INCREMENT,
-    user_id INT(11) NOT NULL,
-    profile_name VARCHAR(60) NOT NULL,
-    profile_image VARCHAR(255) NOT NULL DEFAULT 'placeholder.jpeg',
-    profile_age INT(11) DEFAULT 0,
-    profile_language VARCHAR(60) NOT NULL DEFAULT 'EN',
+    account_id INT(11) NOT NULL,
+    name VARCHAR(60) NOT NULL,
+    image VARCHAR(255) NOT NULL DEFAULT 'placeholder.jpeg',
+    age INT(11) DEFAULT 18,
+    language  ENUM('ENGLISH', 'DUTCH') NOT NULL DEFAULT 'ENGLISH',
     PRIMARY KEY(profile_id),
-    FOREIGN KEY(user_id) REFERENCES User(user_id) ON UPDATE CASCADE ON DELETE NO ACTION
+    FOREIGN KEY(account_id) 
+        REFERENCES Account(account_id) 
+        ON UPDATE CASCADE 
+        ON DELETE CASCADE
 );
 
--- 10 PasswordReset
+-- Profile Preference
+CREATE TABLE IF NOT EXISTS `ProfilePreference` (
+    profile_id INT(11) NOT NULL,
+    classification_id INT(11) NOT NULL,
+    genre_id INT(11) NOT NULL,
+    PRIMARY KEY(profile_id, classification_id, genre_id)
+    FOREIGN KEY(profile_id) 
+        REFERENCES Profile(profile_id) 
+        ON UPDATE CASCADE 
+        ON DELETE CASCADE,
+    FOREIGN KEY(classification_id) 
+        REFERENCES Classification(classification_id) 
+        ON UPDATE CASCADE 
+        ON DELETE CASCADE,
+    FOREIGN KEY(genre_id) 
+        REFERENCES Genre(genre_id) 
+        ON UPDATE CASCADE 
+        ON DELETE CASCADE
+);
+
+-- Invitation
+CREATE TABLE IF NOT EXISTS `Invitation` (
+    invitation_id INT(11) AUTO_INCREMENT NOT NULL,
+    account_id INT(11) NOT NULL,
+    invitee_email VARCHAR(50) UNIQUE NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    accepted_at TIMESTAMP NULL DEFAULT NULL,
+    discount_valid_until TIMESTAMP NULL DEFAULT NULL,
+    status ENUM('PENDING', 'ACCEPTED') NOT NULL DEFAULT 'PENDING',
+    PRIMARY KEY (invitation_id),
+    FOREIGN KEY (account_id) 
+        REFERENCES Account(account_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+-- Movie
+CREATE TABLE IF NOT EXISTS `Movie` (
+    movie_id INT(11) AUTO_INCREMENT,
+    description VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    duration TIME NOT NULL DEFAULT '00:00:00',
+    genre_id INT(11) NOT NULL,
+    classification_id INT(11) NOT NULL,
+    PRIMARY KEY(movie_id),
+    FOREIGN KEY(genre_id) 
+        REFERENCES Genre(genre_id)
+        ON UPDATE CASCADE 
+        ON DELETE CASCADE,
+    FOREIGN KEY(classification_id) 
+        REFERENCES Classification(classification_id)
+        ON UPDATE CASCADE 
+        ON DELETE CASCADE
+);
+
+-- Series
+CREATE TABLE IF NOT EXISTS `Series` (
+    series_id INT(11) AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    seasons INT(11) NOT NULL DEFAULT 1,
+    description VARCHAR(255) NOT NULL,
+    genre_id INT(11) NOT NULL,
+    classification_id INT(11) NOT NULL,
+    PRIMARY KEY(series_id),
+    FOREIGN KEY(genre_id) 
+        REFERENCES Genre(genre_id) 
+        ON UPDATE CASCADE 
+        ON DELETE CASCADE,
+    FOREIGN KEY(classification_id) 
+        REFERENCES Classification(classification_id) 
+        ON UPDATE CASCADE 
+        ON DELETE CASCADE
+);
+
+-- Episode
+CREATE TABLE IF NOT EXISTS `Episode` (
+    episode_id INT(11) AUTO_INCREMENT,
+    series_id INT(11) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    duration TIME NOT NULL DEFAULT '00:00:00',
+    season_number INT(11) NOT NULL,
+    episode_number INT(11) NOT NULL,
+    PRIMARY KEY(episode_id),
+    FOREIGN KEY(series_id) 
+        REFERENCES Series(series_id) 
+        ON UPDATE CASCADE 
+        ON DELETE CASCADE
+);
+
+-- Subtitle
+CREATE TABLE IF NOT EXISTS `Subtitle` (
+    subtitle_id INT(11) AUTO_INCREMENT,
+    movie_id INT(11),
+    episode_id INT(11),
+    language ENUM('ENGLISH', 'DUTCH') NOT NULL DEFAULT 'ENGLISH',
+    subtitle_location VARCHAR(255) NOT NULL,
+    PRIMARY KEY(subtitle_id),
+    FOREIGN KEY(movie_id) 
+        REFERENCES Movie(movie_id)
+        ON UPDATE CASCADE 
+        ON DELETE CASCADE,
+    FOREIGN KEY(episode_id) 
+        REFERENCES Episode(episode_id)
+        ON UPDATE CASCADE 
+        ON DELETE CASCADE,
+    CHECK (
+    (movie_id IS NOT NULL AND episode_id IS NULL) 
+    OR 
+    (movie_id IS NULL AND episode_id IS NOT NULL)
+    )
+);
+
+-- PasswordReset
 CREATE TABLE IF NOT EXISTS `PasswordReset` (
     pass_reset_id INT(11) AUTO_INCREMENT NOT NULL,
-    user_id INT(11) NOT NULL,
-    pass_reset_token LONGTEXT NOT NULL,
+    account_id INT(11) NOT NULL,
+    pass_reset_token VARCHAR(255) NOT NULL,
     PRIMARY KEY(pass_reset_id),
-    FOREIGN KEY(user_id) REFERENCES User(user_id) ON UPDATE CASCADE ON DELETE NO ACTION
+    FOREIGN KEY(account_id) 
+        REFERENCES Account(account_id) 
+        ON UPDATE CASCADE 
+        ON DELETE CASCADE
 );
 
--- 11 Watchlist
+-- Watchlist
 CREATE TABLE IF NOT EXISTS `Watchlist` (
     watchlist_id INT(11) AUTO_INCREMENT,
     profile_id INT(11) NOT NULL,
     name VARCHAR(255) NOT NULL,
     movie_id INT(11),
-    serie_id INT(11),
+    series_id INT(11),
     episode_id INT(11),
     PRIMARY KEY(watchlist_id),
-    FOREIGN KEY(profile_id) REFERENCES Profile(profile_id) ON UPDATE CASCADE ON DELETE NO ACTION,
-    FOREIGN KEY(movie_id) REFERENCES Movie(movie_id) ON UPDATE CASCADE ON DELETE NO ACTION,
-    FOREIGN KEY(serie_id) REFERENCES Serie(serie_id) ON UPDATE CASCADE ON DELETE NO ACTION,
-    FOREIGN KEY(episode_id) REFERENCES Episode(episode_id) ON UPDATE CASCADE ON DELETE NO ACTION
+    FOREIGN KEY(profile_id) 
+        REFERENCES Profile(profile_id) 
+        ON UPDATE CASCADE 
+        ON DELETE NO ACTION,
+    FOREIGN KEY(movie_id) 
+        REFERENCES Movie(movie_id) 
+        ON UPDATE CASCADE 
+        ON DELETE NO ACTION,
+    FOREIGN KEY(series_id) 
+        REFERENCES Series(series_id) 
+        ON UPDATE CASCADE 
+        ON DELETE NO ACTION,
+    FOREIGN KEY(episode_id) 
+        REFERENCES Episode(episode_id) 
+        ON UPDATE CASCADE 
+        ON DELETE NO ACTION,
+    CHECK (movie_id IS NOT NULL OR (series_id IS NOT NULL AND episode_id IS NOT NULL))
 );
 
--- 12 History
-CREATE TABLE IF NOT EXISTS `History` (
+-- Watch History
+CREATE TABLE IF NOT EXISTS `WatchHistory` (
     history_id INT(11) AUTO_INCREMENT,
     profile_id INT(11) NOT NULL,
     movie_id INT(11),
     episode_id INT(11),
-    counter INT(11) NOT NULL DEFAULT 1,
+    duration_watched INT(11) NOT NULL DEFAULT 1,
     PRIMARY KEY(history_id),
-    FOREIGN KEY(profile_id) REFERENCES Profile(profile_id) ON UPDATE CASCADE ON DELETE NO ACTION,
-    FOREIGN KEY(movie_id) REFERENCES Movie(movie_id) ON UPDATE CASCADE ON DELETE NO ACTION,
-    FOREIGN KEY(episode_id) REFERENCES Episode(episode_id) ON UPDATE CASCADE ON DELETE NO ACTION
+    FOREIGN KEY(profile_id) 
+        REFERENCES Profile(profile_id) 
+        ON UPDATE CASCADE 
+        ON DELETE NO ACTION,
+    FOREIGN KEY(movie_id) 
+        REFERENCES Movie(movie_id) 
+        ON UPDATE CASCADE 
+        ON DELETE NO ACTION,
+    FOREIGN KEY(episode_id) 
+        REFERENCES Episode(episode_id) 
+        ON UPDATE CASCADE 
+        ON DELETE NO ACTION,
+    CHECK (movie_id IS NOT NULL OR (series_id IS NOT NULL AND episode_id IS NOT NULL))
 );
